a591880f029eceff40d85a2d1c60d9863ca6cbbc
gomeshenrique <henriquegomes@outlook.com>
2021-06-15 17:01:15 -0300

    adicionar versao estilizada

--- script.js
+++ script.js
@@ -63,6 +63,13 @@ function iniciarJogo() {
     snake[0].y = 16 * box;
   }
 
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+      clearInterval(iniciarJogo);
+      alert("Game Over!");
+    }
+  }
+
   criarBG();
   criarCobrinha();
   drawFood();
